51de3580e2d0372a125b5c3909c30a9937a122a0
prakashsawale <128662417+prakashsawale@users.noreply.github.com>
2023-04-30 07:45:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <body>
+    <center>
+        <h2>Game</h2>
+        <div class="col-lg-4 col-sm-8 col-xs-11 login_div1">
+        <h3>Player 1 login for the game</h3>
+        <label>User Name:</label>
+        <input type="text"id="player1_name_input" class="form-control" placeholder="player1_name"></input>
+        <br></br>
+        </div>
+        <br></br>
+        <div class="col-lg-4 col-sm-8 col-xs-11 login_div1">
+        <h3>Player 2 login for the game</h3>
+        <label>User Name:</label>
+        <input type="text"id="player2_name_input" class="form-control" placeholder="player2_name"></input>
+        <br></br>
+        </div>
+        <br></br>
+    <button style="width:30" class="btn btn-primary" oneclick="adduser()">log in</button>
+        
+    </center>
+</body>
+</head>
+</html>
